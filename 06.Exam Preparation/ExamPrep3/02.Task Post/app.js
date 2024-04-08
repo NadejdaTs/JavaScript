6b47ca33cf1bbf5a5019d41d63f6c0c345caea5a
NadejdaTs <nadezhda.tsvetanova@gmail.com>
2024-04-08 12:47:09 +0300

    Exam prep

--- 06.Exam Preparation/ExamPrep3/02.Task Post/app.js
+++ 06.Exam Preparation/ExamPrep3/02.Task Post/app.js
@@ -12,6 +12,7 @@ function solve() {
     publishButton: document.querySelector("#publish-btn"),
     taskToReview: document.querySelector("#title-bar"),
     reviewList: document.querySelector("#review-list"),
+    publishList: document.querySelector("#published-list"),
   };
 
   selectors.publishButton.addEventListener("click", publishNewTask);
@@ -23,35 +24,69 @@ function solve() {
       category: inputSelectors.category.value,
       content: inputSelectors.content.value,
     };
-    tasks[task.id] = task;
-    const li = createElement("li", null, ["rpost"], null);
-    const article = createElement("article", null, null, null);
 
-    createElement("h4", task.title, null, null, article);
-    createElement("p", task.category, null, null, article);
-    createElement("p", task.content, null, null, article);
-    li.appendChild(article);
+    if (task.title != "" && task.category != "" && task.content != "") {
+      tasks[task.id] = task;
+      const li = createElement("li", null, ["rpost"], null);
+      const article = createElement("article", null, null, null);
 
-    const taskEdit = createElement("button", "Edit", ["action-btn"], null, li);
-    const taskPost = createElement("button", "Post", ["action-btn"], null, li);
+      const h4 = createElement("h4", task.title, null, null, article);
+      const p1 = createElement("p", task.category, null, null, article);
+      const p2 = createElement("p", task.content, null, null, article);
+      li.appendChild(article);
 
-    taskEdit.classList.add("edit");
-    taskEdit.classList.add("post");
-    li.appendChild(taskEdit);
-    li.appendChild(taskPost);
-    selectors.reviewList.appendChild(li);
+      const taskEdit = createElement(
+        "button",
+        "Edit",
+        ["action-btn"],
+        null,
+        li
+      );
+      const taskPost = createElement(
+        "button",
+        "Post",
+        ["action-btn"],
+        null,
+        li
+      );
 
-    clearInputFields();
+      taskEdit.classList.add("edit");
+      taskPost.classList.add("post");
+      li.appendChild(taskEdit);
+      li.appendChild(taskPost);
+      selectors.reviewList.appendChild(li);
+      selectors.publishButton.disabled = true;
+
+      clearInputFields();
+
+      const editButton = document.querySelector(".edit");
+      editButton.addEventListener("click", edit);
+
+      const postButton = document.querySelector(".post");
+      postButton.addEventListener("click", postFn);
+
+      function edit() {
+        inputSelectors.title.value = h4.textContent;
+        inputSelectors.category.value = p1.textContent;
+        inputSelectors.content.value = p2.textContent;
+        selectors.reviewList.removeChild(li);
+        selectors.publishButton.disabled = false;
+      }
+
+      function postFn() {
+        li.removeChild(editButton);
+        li.removeChild(postButton);
+        selectors.reviewList.removeChild(li);
+        selectors.publishList.appendChild(li);
+        selectors.publishButton.disabled = false;
+      }
+    }
   }
 
   function clearInputFields() {
     Object.values(inputSelectors).forEach((input) => {
       input.value = "";
     });
-  }
-
-  function editTask(buttonEdit) {
-    console.log(buttonEdit);
   }
 
   function createElement(type, textContent, classes, id, parent, useInnerHTML) {
