e3afa4240bbcbd582e4a79d1d1833f9ba78840ce
NadejdaTs <nadezhda.tsvetanova@gmail.com>
2024-04-17 23:03:08 +0300

    Exam prep Ex3

--- 06.Exam Preparation/ExamPrep3/03.Course Planner/app.js
+++ 06.Exam Preparation/ExamPrep3/03.Course Planner/app.js
@@ -6,23 +6,38 @@ const inputFields = {
   description: document.querySelector("#description"),
   nameOfTeacher: document.querySelector("#teacher-name"),
 };
+let courses = {};
+let courseIds = [];
 
 function attachEvents() {
   document.querySelector("#add-course").addEventListener("click", addCourse);
-  document.querySelector("#edit-course").addEventListener("click", editCourse);
   document.querySelector("#load-course").addEventListener("click", loadCourses);
 }
 
+function getIdByName(course) {
+  return fetch(API_URL)
+    .then((res) => res.json())
+    .then(
+      (res) => Object.entries(res).find((e) => e[1].title == course)[1]._id
+    );
+}
+
+function clearAllSections() {
+  document.getElementById("list").innerHTML = "";
+}
+
 async function loadCourses() {
-  const response = await fetch(API_URL);
-  const courses = await response.json();
+  clearAllSections();
+  const courses = await (await fetch(API_URL)).json();
 
   const div = document.querySelector("#list");
+
   Object.values(courses).forEach((course) => {
     const container = createElement("div", div, "container", "", "");
     createElement("h2", container, "", "", course.title);
     createElement("h3", container, "", "", course.teacher);
     createElement("h3", container, "", "", course.type);
+
     createElement("h4", container, "", "", course.description);
     const editBtn = createElement(
       "button",
@@ -39,18 +54,21 @@ async function loadCourses() {
       "",
       "Finish Course"
     );
+
+    editBtn.addEventListener("click", editCourse);
+    finishBtn.addEventListener("click", finishCourse);
   });
-  //   editBtn.addEventListener("click", editCourse);
-  //   finishBtn.addEventListener("click", finishCourse);
 }
 async function addCourse() {
   if (Object.values(inputFields).some((input) => input.value === "")) {
     return;
   }
 
+  // clearAllSections();
+
   const currentCourse = {
     title: inputFields.title.value,
-    course: inputFields.type.value,
+    type: inputFields.type.value,
     description: inputFields.description.value,
     teacher: inputFields.nameOfTeacher.value,
   };
@@ -60,31 +78,82 @@ async function addCourse() {
     body: JSON.stringify(currentCourse),
   });
 
+  // clearAllSections();
   loadCourses();
   Object.values(inputFields).forEach((input) => (input.value = ""));
 }
 
-function editCourse() {}
+async function editCourse(e) {
+  const element = e.target.parentNode;
+  const parent = element.parentNode;
 
-function finishCourse() {}
+  inputFields.title.value = element.querySelector("h2").textContent;
+  inputFields.type.value = element.querySelectorAll("h3")[1].innerHTML;
+  inputFields.description.value = element.querySelector("h4").textContent;
+  inputFields.nameOfTeacher.value = element.querySelectorAll("h3")[0].innerHTML;
+
+  parent.removeChild(element);
+  document.querySelector("#add-course").disabled = true;
+  const editBtn = document.querySelector("#edit-course");
+  editBtn.disabled = false;
+
+  loadCourses();
+  editBtn.addEventListener("click", commitChanges);
+}
+
+function finishCourse(event) {
+  const target = event.target.parentElement;
+  const task = Array.from(target.children)[0];
+
+  clearAllSections();
+  getIdByName(task.textContent)
+    .then((id) =>
+      fetch(API_URL + id, {
+        method: "DELETE",
+      })
+    )
+    .then(loadCourses);
+}
+
+async function commitChanges(event) {
+  const target = event.target.parentElement;
+  const task = Array.from(target.children)[1];
+
+  clearAllSections();
+  getIdByName(task.value)
+    .then((id) =>
+      fetch(API_URL + id, {
+        method: "PUT",
+        body: JSON.stringify({
+          title: inputFields.title.value,
+          type: inputFields.type.value,
+          description: inputFields.description.value,
+          teacher: inputFields.nameOfTeacher.value,
+          _id: id,
+        }),
+      })
+    )
+    .then(loadCourses);
+  Object.values(inputFields).forEach((input) => (input.value = ""));
+}
 
 function createElement(element, parent, className, id, textContent) {
   const currElement = document.createElement(element);
-
-  if (parent) {
-    parent.appendChild(currElement);
-  }
 
   if (className) {
     currElement.classList.add(className);
   }
 
   if (id) {
-    currElement.setAtribute("id", id);
+    currElement.setAttribute("id", id);
   }
 
   if (textContent) {
     currElement.textContent = textContent;
+  }
+
+  if (parent) {
+    parent.appendChild(currElement);
   }
   return currElement;
 }
---
+++ 06.Exam Preparation/ExamPrep4/03.Vacation Schedule/app.js
@@ -0,0 +1,160 @@
+attachEvents();
+const API_URL = "http://localhost:3030/jsonstore/tasks/";
+
+let inputFields = {
+  name: document.querySelector("#name"),
+  days: document.querySelector("#num-days"),
+  date: document.querySelector("#from-date"),
+};
+
+function attachEvents() {
+  document
+    .querySelector("#load-vacations")
+    .addEventListener("click", loadVacations);
+  document
+    .querySelector("#add-vacation")
+    .addEventListener("click", addVacations);
+}
+
+function clearInputFields() {
+  Object.values(inputFields).forEach((input) => (input.value = ""));
+}
+
+function clearAllSections() {
+  document.getElementById("list").innerHTML = "";
+}
+
+function getIdByName(vacationName) {
+  return fetch(API_URL)
+    .then((res) => res.json())
+    .then(
+      (res) => Object.entries(res).find((e) => e[1].name == vacationName)[1]._id
+    );
+}
+
+async function loadVacations() {
+  clearAllSections();
+  const vacations = await (await fetch(API_URL)).json();
+
+  const list = document.querySelector("#list");
+  Object.values(vacations).forEach((vacation) => {
+    const container = createElement("div", list, "", "container", "");
+    createElement("h2", container, vacation.name);
+    createElement("h3", container, vacation.date);
+    createElement("h3", container, vacation.days);
+    const changeBtn = createElement(
+      "button",
+      container,
+      "Change",
+      "change-btn"
+    );
+    const doneBtn = createElement("button", container, "Done", "done-btn");
+
+    changeBtn.addEventListener("click", changeVacation);
+    doneBtn.addEventListener("click", doneVacation);
+  });
+
+  document.querySelector("#edit-vacation").style.disabled = true;
+}
+
+async function addVacations(event) {
+  event.preventDefault();
+  if (Object.values(inputFields).some((input) => input.value === "")) {
+    return;
+  }
+
+  const currentVacation = {
+    name: inputFields.name.value,
+    days: inputFields.days.value,
+    date: inputFields.date.value,
+  };
+
+  console.log(currentVacation);
+
+  await fetch(API_URL, {
+    method: "POST",
+    body: JSON.stringify(currentVacation),
+  });
+
+  fetch(API_URL).then(loadVacations()).catch(console.error);
+  clearInputFields();
+}
+
+async function changeVacation(event) {
+  const element = event.target.parentNode;
+  const parent = element.parentNode;
+
+  inputFields.name.value = element.querySelector("h2").textContent;
+  inputFields.days.value = element.querySelectorAll("h3")[1].innerHTML;
+  inputFields.date.value = element.querySelectorAll("h3")[0].innerHTML;
+
+  parent.removeChild(element);
+  document.querySelector("#add-vacation").disabled = true;
+  const editBtn = document.querySelector("#edit-vacation");
+  editBtn.disabled = false;
+
+  editBtn.addEventListener("click", commitChanges);
+}
+
+async function commitChanges(event) {
+  const target = event.target.parentElement;
+  const task = Array.from(target.children)[1];
+
+  event.preventDefault();
+  // const data = { name: nameElement.value, days: daysNumber.value, date: dateElement.value};
+  getIdByName(task.value).then((id) =>
+    fetch(API_URL + id, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: inputFields.name.value,
+        days: inputFields.days.value,
+        date: inputFields.date.value,
+        _id: id,
+      }),
+    })
+  );
+
+  document.querySelector("#add-vacation").disabled = false;
+  document.querySelector("#edit-vacation").disabled = true;
+
+  fetch(API_URL).then(loadVacations()).catch(console.error);
+  clearInputFields();
+}
+
+async function doneVacation(event) {
+  const target = event.target.parentElement;
+  const task = Array.from(target.children)[0];
+
+  getIdByName(task.textContent).then((id) =>
+    fetch(API_URL + id, {
+      method: "DELETE",
+      headers: { "content-type": "application/json" },
+    })
+  );
+
+  fetch(API_URL).then(loadVacations()).catch(console.error);
+}
+
+function createElement(type, parent, textContent, classes, id) {
+  const element = document.createElement(type);
+
+  if (textContent) {
+    element.textContent = textContent;
+  }
+
+  if (classes) {
+    element.classList.add(classes);
+  }
+
+  if (id) {
+    element.setAttribute("id", id);
+  }
+
+  if (parent) {
+    parent.appendChild(element);
+  }
+  return element;
+}
